97ce9f3174573d2f57df46df858e7f1a5018667f
Daniel Lucas <dlpsilva@yahoo.com.br>
2022-06-13 18:41:00 -0300

    Ajustes Fornecedor Controller add Services de endereço

---
+++ src/EasyList.Business/Interfaces/IServices/IEnderecoService.cs
@@ -0,0 +1,17 @@
+﻿using EasyList.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyList.Business.Interfaces.IServices
+{
+    public interface IEnderecoService : IDisposable
+    {
+        Task<bool> Adicionar(Endereco endereco);
+        Task<bool> Atualizar(Endereco endereco);
+        Task<bool> Remover(Guid id);
+        Task<Endereco> ObterEnderecoPorId(Guid Id);
+    }
+}
---
+++ src/EasyList.Business/Services/EnderecoService.cs
@@ -0,0 +1,63 @@
+﻿using EasyList.Business.Interfaces.IRepository;
+using EasyList.Business.Interfaces.IServices;
+using EasyList.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyList.Business.Services
+{
+    public class EnderecoService : IEnderecoService
+    {
+        public IEnderecoRepository _enderecoRepository { get; set; }
+        public EnderecoService(IEnderecoRepository enderecoRepository)
+        {
+            _enderecoRepository = enderecoRepository;
+        }      
+                
+        public async Task<bool> Adicionar(Endereco endereco)
+        {
+            if (!await EnderecoExists(endereco.Id))
+                return false;
+
+            await _enderecoRepository.Adicionar(endereco);
+            return true;
+        }             
+
+        public async Task<bool> Atualizar(Endereco endereco)
+        {
+            if (!await EnderecoExists(endereco.Id))
+                return false;
+
+            await _enderecoRepository.Atualizar(endereco);
+            return true;
+        }
+
+        public async Task<bool> Remover(Guid id)
+        {
+            if (!await EnderecoExists(id))
+                return false;
+
+            await _enderecoRepository.Remover(id);
+            return true;
+        }
+
+        public async Task<Endereco> ObterEnderecoPorId(Guid Id)
+        {
+            return await _enderecoRepository.ObterPorId(Id);
+        }
+
+        private Task<bool> EnderecoExists(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _enderecoRepository?.Dispose();
+        }
+
+    }
+}
